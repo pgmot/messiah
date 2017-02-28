ec32c567b64a7b86a71e9086bbbef7d6429f67b1
mot <mot.contact.address.pc@gmail.com>
2017-02-28 16:50:08 +0900

    細かい説明追加

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 ```
 bundle exec sidekiq -r ./config/boot.rb -C config/sidekiq.yml -d
 ```
+
+```
+bundle exec padrino rake ar:create ar:migrate
+bundle exec padrino rake ar:migrate
+```
+
+```
+mkdir -p tmp/pids tmp/sockets
+bundle exec unicorn -c config/unicorn.rb -e production -d
+kill -9 `cat tmp/pids/unicorn.pid`
+```
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -25,7 +25,7 @@ stdout_path File.expand_path('../../log/unicorn_stdout.log', __FILE__)
 # Nginxで使用する場合は以下の設定を行う。
 # listen File.expand_path('../../tmp/sockets/unicorn.sock', __FILE__)
 # とりあえず起動して動作確認をしたい場合は以下の設定を行う。
-listen 8080
+listen File.expand_path('../../tmp/sockets/unicorn.sock', __FILE__)
 # ※「backlog」や「tcp_nopush」の設定もあるけど、よくわかって無い。
 
 # プロセスの停止などに必要なPIDファイルの保存先を指定。
